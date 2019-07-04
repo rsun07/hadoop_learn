e174e251e3d6375527a5f804c16555b876ad153c
Xiaoming Sun <rsun2014@gmail.com>
2019-07-04 16:21:45 -0700

    file buffer and seek

--- hdfs/src/test/java/pers/xiaoming/hadoop/FSDataStream.java
+++ hdfs/src/test/java/pers/xiaoming/hadoop/FSDataStream.java
@@ -33,5 +33,36 @@ public class FSDataStream extends HdfsDemoBase {
         FileOutputStream fileOutputStream = new FileOutputStream(new File(LOCAL_DOWNLOAD_FILE_PATH));
 
         IOUtils.copyBytes(fsDataInputStream, fileOutputStream, bufferSize);
+
+        IOUtils.closeStream(fileOutputStream);
+        IOUtils.closeStream(fsDataInputStream);
+    }
+
+    @Test
+    public void fileBufferAndSeek() throws IOException {
+        FSDataInputStream fsDataInputStream = fs.open(new Path(HDFS_DOWNLOAD_FILE_PATH));
+
+        FileOutputStream fileOutputStream = new FileOutputStream(new File(LOCAL_DOWNLOAD_FILE_PATH));
+
+        byte[] buffer = new byte[bufferSize];
+
+        for (int i = 0; i < bufferSize * 2; i++) {
+            fsDataInputStream.read(buffer);
+            fileOutputStream.write(buffer);
+        }
+
+        fsDataInputStream.close();
+        fileOutputStream.close();
+
+        fsDataInputStream = fs.open(new Path(HDFS_DOWNLOAD_FILE_PATH));
+        fsDataInputStream.seek(bufferSize * 2);
+        fileOutputStream = new FileOutputStream(new File(LOCAL_DOWNLOAD_FILE_PATH));
+
+        while(fsDataInputStream.read(buffer) > 0) {
+            fileOutputStream.write(buffer);
+        }
+
+        fsDataInputStream.close();
+        fileOutputStream.close();
     }
 }
