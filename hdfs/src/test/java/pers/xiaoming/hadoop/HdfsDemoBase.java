fd92caeb2098be5dece7cf88d57ca99d4b5ac248
Xiaoming Sun <rsun2014@gmail.com>
2019-07-04 14:07:06 -0700

    HDFS demo base

---
+++ hdfs/src/test/java/pers/xiaoming/hadoop/HdfsDemoBase.java
@@ -0,0 +1,29 @@
+package pers.xiaoming.hadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+import java.io.IOException;
+
+public class HdfsDemoBase {
+    private static final String DEMO_DEFAULT_FS = "hdfs://localhost:9000";
+
+    protected static FileSystem fs;
+
+
+    @BeforeClass
+    public static void setup() throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS", DEMO_DEFAULT_FS);
+        fs = FileSystem.get(configuration);
+    }
+
+    @AfterClass
+    public static void close() throws IOException {
+        if (fs != null) {
+            fs.close();
+        }
+    }
+}
