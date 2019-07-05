522584e5a619a58d448e4c3fac7b76d18ccea6ef
Xiaoming Sun <rsun2014@gmail.com>
2019-07-04 20:53:59 -0700

    Zookeeper dynamic config puller impl

---
+++ zookeeper/src/main/java/pers.xiaoming.hadoop.zookeeper/DynamicConfigPuller.java
@@ -0,0 +1,50 @@
+package pers.xiaoming.hadoop.zookeeper;
+
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DynamicConfigPuller {
+    private static final Logger logger = Logger.getLogger(DynamicConfigPuller.class);
+    private final ZooKeeper zooKeeper;
+    private Map<String, String> configs;
+
+    private Watcher configUpdateWatcher = new Watcher() {
+        @Override
+        public void process(WatchedEvent event) {
+            if (event.getType() == Event.EventType.NodeDataChanged) {
+                configUpdate(event.getPath());
+            }
+        }
+    };
+
+    public DynamicConfigPuller(ZooKeeper zooKeeper, String[] keys) throws KeeperException, InterruptedException {
+        this.zooKeeper = zooKeeper;
+        initalConfigs(keys);
+    }
+
+    private void initalConfigs(String[] keys) throws KeeperException, InterruptedException {
+        this.configs = new ConcurrentHashMap<>();
+        for (String key : keys) {
+            zooKeeper.getData(key, configUpdateWatcher, null);
+        }
+    }
+
+    private void configUpdate(String key) {
+        String value = null;
+        try {
+            byte[] data = zooKeeper.getData(key, false, null);
+            value = new String(data);
+        } catch (KeeperException e) {
+            logger.error("Zookeeper data not found for: " + key);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        configs.put(key, value);
+    }
+}
