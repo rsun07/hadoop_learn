678c02bf6a857ea3794e75f1474768481e59925b
Xiaoming Sun <rsun2014@gmail.com>
2019-07-05 11:05:14 -0700

    LeaderLatchClient impl

--- zookeeper/src/main/java/pers.xiaoming.hadoop.zookeeper/leader/LeaderLatchClient.java
+++ zookeeper/src/main/java/pers.xiaoming.hadoop.zookeeper/leader/LeaderLatchClient.java
@@ -1,4 +1,52 @@
 package pers.xiaoming.hadoop.zookeeper.leader;
 
-public class LeaderLatchClient {
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.leader.LeaderLatch;
+import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class LeaderLatchClient implements LeaderLatchListener {
+    private static final Logger logger = Logger.getLogger(LeaderLatchClient.class);
+
+    private final String name;
+
+    private LeaderLatch leaderLatch;
+
+    private CuratorFramework client;
+
+    public LeaderLatchClient(String name, String latchPath, CuratorFramework client) throws Exception {
+        this.name = name;
+        this.client = client;
+        this.leaderLatch = new LeaderLatch(client, latchPath);
+        leaderLatch.addListener(this);
+        leaderLatch.start();
+    }
+
+    @Override
+    public void isLeader() {
+        logger.info(name + " is elected to be leader");
+    }
+
+    @Override
+    public void notLeader() {
+        logger.info(name + " is no longer the leader");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CuratorFramework getClient() {
+        return client;
+    }
+
+    public void stop() throws IOException {
+        leaderLatch.close();
+    }
+
+    public boolean hasLeadership() {
+        return leaderLatch.hasLeadership();
+    }
 }
