62f5c3041aa7739b5a14ec8188948bf4fe3cad28
Xiaoming Sun <rsun2014@gmail.com>
2019-07-06 14:03:39 -0700

    Partition test improve output path to make different test diff path

--- mapreduce/src/test/java/pers.xiaoming.hadoop.mapreduce/payment/PartitionerTest.java
+++ mapreduce/src/test/java/pers.xiaoming.hadoop.mapreduce/payment/PartitionerTest.java
@@ -9,17 +9,20 @@ import java.io.IOException;
 
 public class PartitionerTest extends TestBase {
     private static final String RESOURCES_PATH = ROOT_RESOURCE_PATH + "/payment";
-    private static final String FIVE_K_INPUT_PATH = RESOURCES_PATH + "/payment_input_5k";
-    private static final String FIVE_K_OUTPUT_PATH = RESOURCES_PATH + "/partitioner_output_5k";
+    private static final String INPUT_PATH = RESOURCES_PATH + "/payment_input_5k";
+    private static final String OUTPUT_PATH_PREFIX = RESOURCES_PATH + "/partitioner_output_5k";
+    private static final String NUM_PARTITION_ONE_OUTPUT_PATH = OUTPUT_PATH_PREFIX + "_num_p_1";
+    private static final String NUM_PARTITION_FIVE_OUTPUT_PATH = OUTPUT_PATH_PREFIX + "_num_p_5";
 
     @BeforeClass
     public static void cleanup() throws IOException {
-        cleanupOutputDir(FIVE_K_OUTPUT_PATH);
+        cleanupOutputDir(NUM_PARTITION_ONE_OUTPUT_PATH);
+        cleanupOutputDir(NUM_PARTITION_FIVE_OUTPUT_PATH);
     }
 
     @Test
     public void testOnePartition() throws IOException, ClassNotFoundException, InterruptedException {
-        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(FIVE_K_INPUT_PATH, FIVE_K_OUTPUT_PATH);
+        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(INPUT_PATH, NUM_PARTITION_ONE_OUTPUT_PATH);
         Assert.assertTrue(driver.run());
     }
 
@@ -27,13 +30,13 @@ public class PartitionerTest extends TestBase {
     public void testThreePartition() throws IOException, ClassNotFoundException, InterruptedException {
         // partition number is bigger than 1 and lesser than num of partition
         // exception will throw
-        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(FIVE_K_INPUT_PATH, FIVE_K_OUTPUT_PATH, 3);
+        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(INPUT_PATH, OUTPUT_PATH_PREFIX + "_3", 3);
         Assert.assertFalse(driver.run());
     }
 
     @Test
     public void testFivePartition() throws IOException, ClassNotFoundException, InterruptedException {
-        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(FIVE_K_INPUT_PATH, FIVE_K_OUTPUT_PATH, 5);
+        PaymentPartitionCountDriver driver = new PaymentPartitionCountDriver(INPUT_PATH, NUM_PARTITION_FIVE_OUTPUT_PATH, 5);
         Assert.assertTrue(driver.run());
     }
 }
