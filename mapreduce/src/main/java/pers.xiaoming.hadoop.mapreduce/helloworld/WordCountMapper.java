e3ec2f868bb3b3060f002650cf34250d584ce7b5
Xiaoming Sun <rsun2014@gmail.com>
2019-07-06 10:42:47 -0700

    comment for default FileInputFormat, TextInputFormat

--- mapreduce/src/main/java/pers.xiaoming.hadoop.mapreduce/helloworld/WordCountMapper.java
+++ mapreduce/src/main/java/pers.xiaoming.hadoop.mapreduce/helloworld/WordCountMapper.java
@@ -7,6 +7,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
+// Why input format is <LongWritable, Text>?
+// Because it uses default TextInputFormat (default impl of FileInputFormat)
+// the 'LongWritable' is the offset of each line's first char in the whole document
+// the 'Text' is the text of each line
 public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     Text k = new Text();
