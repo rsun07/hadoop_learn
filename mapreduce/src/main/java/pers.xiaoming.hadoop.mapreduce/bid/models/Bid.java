1b1cd3d378023766c9def1e4a5e31bed5d815dbf
Xiaoming Sun <rsun2014@gmail.com>
2019-07-06 18:41:15 -0700

    Bid bean

---
+++ mapreduce/src/main/java/pers.xiaoming.hadoop.mapreduce/bid/models/Bid.java
@@ -0,0 +1,43 @@
+package pers.xiaoming.hadoop.mapreduce.bid.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bid implements WritableComparable<Bid> {
+    private int targetId;
+    private double price;
+
+    @Override
+    public int compareTo(Bid that) {
+        double diff = this.price - that.getPrice();
+
+        if (diff > 0) {
+            return 1;
+        } else if (diff == 0) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeInt(targetId);
+        dataOutput.writeDouble(price);
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.targetId = dataInput.readInt();
+        this.price = dataInput.readDouble();
+    }
+}
